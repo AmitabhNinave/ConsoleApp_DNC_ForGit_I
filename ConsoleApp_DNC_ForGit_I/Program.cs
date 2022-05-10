3571c517d22e19d78f1c707d78cf02dbef461619
Amitabh Ninave <amitabh.ninave@hotmail.com>
2022-05-10 13:29:28 +0530

    new file:   ConsoleApp_DNC_ForGit_I/obj/project.assets.json

---
+++ ConsoleApp_DNC_ForGit_I/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp_DNC_ForGit_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World II!");
+        }
+    }
+}
